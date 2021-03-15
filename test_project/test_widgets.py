b0fd942fb6c32f9ca0ee933e646917b4b80567ad
Simon Willison <swillison@gmail.com>
2021-03-14 17:51:00 -0700

    Stop using intcomma in big_number widget
    I don't want to have to tell users to install django.contrib.humanize

--- test_project/test_widgets.py
+++ test_project/test_widgets.py
@@ -1,5 +1,7 @@
 def test_big_number_widget(admin_client, dashboard_db):
-    response = admin_client.post("/dashboard/", {
-        "sql": "select 'Big' as label, 10801 * 5 as big_number"
-    }, follow=True)
-    assert b'<p>Big</p>\n  <h1>54,005</h1>' in response.content
+    response = admin_client.post(
+        "/dashboard/",
+        {"sql": "select 'Big' as label, 10801 * 5 as big_number"},
+        follow=True,
+    )
+    assert b"<p>Big</p>\n  <h1>54005</h1>" in response.content

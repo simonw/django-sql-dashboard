e9204d82860f2acfb5ef55c6ac3aaacb7e951068
Simon Willison <swillison@gmail.com>
2021-04-13 22:53:01 -0700

    Test confirming user_can_execute_sql check, closes #55

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.contrib.auth.models import Permission
 
 
 @pytest.fixture
@@ -6,3 +7,12 @@ def dashboard_db(settings, db):
     settings.DATABASES["dashboard"]["OPTIONS"] = {
         "options": "-c default_transaction_read_only=on -c statement_timeout=100"
     }
+
+
+@pytest.fixture
+def execute_sql_permission():
+    return Permission.objects.get(
+        content_type__app_label="django_sql_dashboard",
+        content_type__model="dashboard",
+        codename="execute_sql",
+    )
--- test_project/test_dashboard_permissions.py
+++ test_project/test_dashboard_permissions.py
@@ -18,12 +18,9 @@ def test_superusers_allowed(admin_client, dashboard_db):
     assert b"<title>Django SQL Dashboard</title>" in response.content
 
 
-def test_must_have_execute_sql_permission(client, django_user_model, dashboard_db):
-    execute_sql = Permission.objects.get(
-        content_type__app_label="django_sql_dashboard",
-        content_type__model="dashboard",
-        codename="execute_sql",
-    )
+def test_must_have_execute_sql_permission(
+    client, django_user_model, dashboard_db, execute_sql_permission
+):
     not_staff = django_user_model.objects.create(username="not_staff")
     staff_no_permisssion = django_user_model.objects.create(
         username="staff_no_permission", is_staff=True
@@ -31,7 +28,7 @@ def test_must_have_execute_sql_permission(client, django_user_model, dashboard_d
     staff_with_permission = django_user_model.objects.create(
         username="staff_with_permission", is_staff=True
     )
-    staff_with_permission.user_permissions.add(execute_sql)
+    staff_with_permission.user_permissions.add(execute_sql_permission)
     assert staff_with_permission.has_perm("django_sql_dashboard.execute_sql")
     client.force_login(not_staff)
     assert client.get("/dashboard/").status_code == 302
@@ -39,6 +36,24 @@ def test_must_have_execute_sql_permission(client, django_user_model, dashboard_d
     assert client.get("/dashboard/").status_code == 302
     client.force_login(staff_with_permission)
     assert client.get("/dashboard/").status_code == 200
+
+
+def test_user_without_execute_sql_permission_does_not_see_count_links_on_saved_dashboard(
+    client, django_user_model, execute_sql_permission, dashboard_db
+):
+    dashboard = Dashboard.objects.create(slug="test", view_policy="public")
+    dashboard.queries.create(sql="select 11 + 34")
+    user = django_user_model.objects.create(username="regular")
+    client.force_login(user)
+    response = client.get("/dashboard/test/")
+    assert response.status_code == 200
+    html = response.content.decode("utf-8")
+    assert ">count<" not in html
+    # If the user DOES have that permission they get the count links
+    user.user_permissions.add(execute_sql_permission)
+    response = client.get("/dashboard/test/")
+    html = response.content.decode("utf-8")
+    assert ">count<" in html
 
 
 def test_saved_dashboard_anonymous_users_denied_by_default(client, dashboard_db):
